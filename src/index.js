aae53d88dc27f148f9f381e19b36d161febb9839
Artkvadrat <Artkvadrat984@gmail.com>
2019-12-05 20:22:46 +0200

    A final version of Redux-SandBox.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from "./counter";
-import { createStore, bindActionCreators } from 'redux';
+import App from "./components/app";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from "./reducer";
 //importing all actions from file
-import * as actions from "./actions";
 
 const store = createStore(reducer);
-const { dispatch } = store;
+// const { dispatch } = store;
 //shorter versions of calling dispatch by using a redux functions bindActionCreators
-const { inc, dec, rnd } = bindActionCreators( actions, dispatch);
+// const { inc, dec, rnd } = bindActionCreators( actions, dispatch);
 
-const update = () => {
-    ReactDOM.render( < Counter
-            counter={ store.getState()}
-            inc={ inc }
-            dec={ dec }
-            rnd={ () => {
-                const value = Math.floor( Math.random() * 10 );
-                rnd(value);
-            } } />,
-        document.getElementById('root'));
-};
-update();
-store.subscribe( update );
+
+ReactDOM.render(
+    <Provider store={ store }>
+        < App />
+    </Provider>,
+    document.getElementById('root'));
+
 
